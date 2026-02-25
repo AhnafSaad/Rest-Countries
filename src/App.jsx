38974092e9fd7b23d37fddeff163dd8e6300f632
Saad <saad35-892@diu.edu.bd>
2026-02-26 01:58:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Countries from "./components/countries/Countries";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { useState } from "react";
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>React World On The Go</h1>
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
